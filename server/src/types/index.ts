251b3b23bc35df3f6b9079562988b6573095bd14
Никита <nitina21052008@gmail.com>
2026-03-10 13:30:32 +0300

    feat: TL base setup (Server, Client, DB Service, Strict Types)

---
+++ server/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export interface User {
+  id: string;
+  login: string;
+  email: string;
+}
